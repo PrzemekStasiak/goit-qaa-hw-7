b8dc5edc0f26c547dad2b19bdfd6bc40ac434764
PrzemekStasiak <140719984+PrzemekStasiak@users.noreply.github.com>
2024-04-20 21:10:51 +0200

    last cosmetiv changes nad with new mothod task 3

--- js/task-03.js
+++ js/task-03.js
@@ -1,19 +1,19 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
 const gallery = document.querySelector(".gallery"); // ustawienie parent na wyszukanym id gallery
-images.forEach((image) => {  //dla każdego elementu image z images
-  let tekst = (`<li><img><img src = ${image.url}> alt = "${image.alt}"</img></li>`) //stówrz tekst o podanym formacie
-  gallery.insertAdjacentHTML("beforeend", tekst)/// dodaj w dziecku na końcu łańcucha ww. tekst
-})
+const items = images.map((image) => { //mapowanie każdego elementu images
+    return `<li><img src=${image.url} /> alt ="${image.alt}" </li>`; //zwróć do tablicy o podanej frazie
+  }).join(" "); // wywołaj funkcję join
+gallery.insertAdjacentHTML("beforeend", items); /// dodaj w dziecku na końcu łańcucha ww. stworzoną nową tablicę i sformatowaną do HTML
